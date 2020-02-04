a8f96238c955b6134ac6f72e57eae3cfa3e92a16
Eugeniu Ignat <eugeniu.ignat@amdaris.com>
2020-02-04 14:52:20 +0200

    Added new line 6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World! 4");
             Console.WriteLine("Hello World! 3");
             Console.WriteLine("Hello World! 5");
+            Console.WriteLine("Hello World! 6");
+
         }
     }
 }

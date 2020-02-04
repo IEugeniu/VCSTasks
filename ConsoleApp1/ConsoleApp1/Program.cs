49a085920183e80899dd09e4cec0f69eab320ace
Eugeniu Ignat <eugeniu.ignat@amdaris.com>
2020-02-04 14:32:07 +0200

    Text conflict solved by keeping both changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,11 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World! 2");
-<<<<<<< HEAD
             Console.WriteLine("Hello World! 4");
-=======
             Console.WriteLine("Hello World! 3");
->>>>>>> bbd3cee0c7603a3c8239123e11b8de557a5f6539
         }
     }
 }
